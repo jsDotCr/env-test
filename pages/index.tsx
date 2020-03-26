16ed2d0cf8d9b84cf7bd344afab540246815d86c
jsDotCr <rocco.curcio@blacklane.com>
2020-03-26 15:56:25 +0100

    yet another PR, as I've never set up a default branch

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Index () {
       </p>
       <style>{`
         .hello {
-          font: 16px Helvetica, Arial, sans-serif;
+          font: 16px Comic Sans MS, Comic Sans, cursive;
           background: #eee;
           padding: 100px;
           text-align: center;
